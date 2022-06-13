da558217e788e78f4264e8467cc96bec413c5348
ItsRifter <d_thomas_smith30@hotmail.com>
2022-06-13 16:11:01 +0100

    Few simple player functions
    Added simple player stuff
    Added respawn/suicide cooldowns

---
+++ code/PCGame.cs
@@ -0,0 +1,38 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public partial class PCGame : Game
+{
+	public PCGame()
+	{
+
+	}
+
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new PCPawn();
+		pawn.Spawn();
+
+		client.Pawn = pawn;
+	}
+	public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
+	{
+		base.ClientDisconnect( cl, reason );
+	}
+
+	public override void DoPlayerSuicide( Client cl )
+	{
+		if(cl.Pawn is PCPawn player)
+		{
+			if( player.timeLastRespawn >= 3.0f)
+				base.DoPlayerSuicide( cl );
+		}
+	}
+}
---
+++ code/PCPawn.cs
@@ -0,0 +1,153 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+
+partial class PCPawn : Player
+{
+
+	DamageInfo lastDMGInfo;
+
+	//Don't allow players to spam death and respawning
+	TimeSince timeLastDied;
+	public TimeSince timeLastRespawn;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		//Temporary, should have a custom camera
+		CameraMode = new FirstPersonCamera();
+
+		Animator = new StandardPlayerAnimator();
+
+		//Should we have our own walk controller?
+		Controller = new WalkController();
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+
+		//Use the base player respawn, NOT the respawn in this class
+		base.Respawn();
+	}
+
+	public override void Respawn()
+	{
+		base.Respawn();
+		CameraMode = new FirstPersonCamera();
+
+		EnableAllCollisions = true;
+		EnableDrawing = true;
+
+		//Deletes the corpse if valid
+		if( Corpse.IsValid() )
+		{
+			Corpse.Delete();
+			Corpse = null;
+		}
+
+		timeLastRespawn = 0;
+	}
+
+	//Simulation on both server and client
+	public override void Simulate( Client cl )
+	{
+		//We won't call the base simulate since it automatically respawns the player on death in a few seconds
+		//but use the last 2 lines of it
+
+		var controller = GetActiveController();
+		controller?.Simulate( cl, this, GetActiveAnimator() );
+
+		if ( IsServer )
+		{
+			if ( LifeState == LifeState.Alive )
+				SimulateActions();
+			else if ( LifeState == LifeState.Dead )
+				SimulateActionsWhilstDead();
+		}
+	}
+
+	public void SimulateActions()
+	{
+
+	}
+
+	public void SimulateActionsWhilstDead()
+	{
+		if(Input.Pressed(InputButton.PrimaryAttack) && timeLastDied > 3.0f)
+			Respawn();
+	}
+
+	//When the player is killed
+	public override void OnKilled()
+	{
+		base.OnKilled();
+
+		EnableAllCollisions = false;
+		EnableDrawing = false;
+
+		CreatePlayerRagdoll( lastDMGInfo.Force, lastDMGInfo.BoneIndex );
+
+		//We should make a first person death camera in the future
+		CameraMode = new SpectateRagdollCamera();
+
+		timeLastDied = 0;
+	}
+
+	//Frame simulated on the client
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+	}
+
+	//Creates a player ragdoll with clothing (if any) at force with bone index
+	public void CreatePlayerRagdoll( Vector3 force, int forceBone )
+	{
+		var ent = new ModelEntity();
+		ent.Position = Position;
+		ent.Rotation = Rotation;
+		ent.MoveType = MoveType.Physics;
+		ent.UsePhysicsCollision = true;
+		ent.SetInteractsAs( CollisionLayer.Debris );
+		ent.SetInteractsWith( CollisionLayer.WORLD_GEOMETRY );
+		ent.SetInteractsExclude( CollisionLayer.Player | CollisionLayer.Debris );
+
+		ent.CopyFrom( this );
+		ent.CopyBonesFrom( this );
+		ent.SetRagdollVelocityFrom( this );
+
+		Corpse = ent;
+
+		// Copy the clothes over
+		foreach ( var child in Children )
+		{
+			if ( !child.Tags.Has( "clothes" ) )
+				continue;
+
+			if ( child is ModelEntity e )
+			{
+				var clothing = new ModelEntity();
+				clothing.CopyFrom( e );
+				clothing.SetParent( ent, true );
+			}
+		}
+
+		ent.PhysicsGroup.AddVelocity( force );
+
+		if ( forceBone >= 0 )
+		{
+			var body = ent.GetBonePhysicsBody( forceBone );
+			if ( body != null )
+			{
+				body.ApplyForce( force * 1000 );
+			}
+			else
+			{
+				ent.PhysicsGroup.AddVelocity( force );
+			}
+		}
+	}
+}
