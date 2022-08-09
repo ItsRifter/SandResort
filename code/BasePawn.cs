573a143fe1043b8f4b3c9a58557e64240e14179b
ItsRifter <d_thomas_smith30@hotmail.com>
2022-08-10 00:03:06 +0100

    Delete majority of the code

---
+++ code/BasePawn.cs
@@ -0,0 +1,182 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//Base of PlayHome's player pawns
+public partial class BasePawn : Player
+{
+	DamageInfo lastDMGInfo;
+
+	bool UpdateViewAngle;
+	Angles UpdatedViewAngle;
+	public BasePawn()
+	{
+
+	}
+
+	public void SetUpPlayer()
+	{
+		Host.AssertServer();
+
+		LifeState = LifeState.Alive;
+		Health = 100;
+		Velocity = Vector3.Zero;
+		WaterLevel = 0;
+
+		ResetInterpolation();
+
+		SetModel( "models/citizen/citizen.vmdl" );
+
+		//Temporary, should have a custom camera
+		CameraMode = new FirstPersonCamera();
+
+		Animator = new StandardPlayerAnimator();
+
+		//Should we have our own walk controller?
+		Controller = new WalkController();
+
+		EnableDrawing = true;
+		EnableHideInFirstPerson = true;
+		EnableShadowInFirstPerson = true;
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetUpPlayer();
+
+		//Use the base player respawn, NOT the respawn in this class
+		base.Respawn();
+	}
+
+	public override void Respawn()
+	{
+		base.Respawn();
+
+		SetUpPlayer();
+
+		//Deletes the corpse if valid
+		DestroyCorpse(To.Everyone);
+	}
+
+	//Thanks Crayz
+	[ClientRpc]
+	public void SetViewAngles( Angles angles )
+	{
+		UpdateViewAngle = true;
+		UpdatedViewAngle = angles;
+	}
+
+	public override void BuildInput( InputBuilder input )
+	{
+		base.BuildInput( input );
+
+		if ( UpdateViewAngle )
+		{
+			UpdateViewAngle = false;
+			input.ViewAngles = UpdatedViewAngle;
+		}
+	}
+
+	//Simulation on both server and client
+	public override void Simulate( Client cl )
+	{
+		//We won't call the base simulate since it automatically respawns the player on death in a few seconds
+		//but use the last 2 lines of it
+
+		var controller = GetActiveController();
+		controller?.Simulate( cl, this, GetActiveAnimator() );
+
+		if ( cl.GetClientData( "cl_showfps", false ) )
+		{
+			var fps = 1 / RealTime.Delta;
+			DebugOverlay.ScreenText( $"{fps.ToString( "0.00" )} FPS", -2 );
+		}
+	}
+
+	public override void TakeDamage( DamageInfo info )
+	{
+		lastDMGInfo = info;
+		base.TakeDamage( info );
+	}
+
+	//When the player is killed
+	public override void OnKilled()
+	{
+		base.OnKilled();
+
+		EnableAllCollisions = false;
+		EnableDrawing = false;
+
+		CreatePlayerRagdoll( lastDMGInfo.Force, lastDMGInfo.BoneIndex );
+	}
+
+	//Frame simulated on the client
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+	}
+
+	public override void OnAnimEventFootstep( Vector3 pos, int foot, float volume )
+	{
+		base.OnAnimEventFootstep( pos, foot, volume * 10 );
+	}
+
+	//Creates a player ragdoll with clothing (if any) at force with bone index
+	[ClientRpc]
+	public void CreatePlayerRagdoll( Vector3 force, int forceBone )
+	{
+		var ent = new ModelEntity();
+		ent.Position = Position;
+		ent.Rotation = Rotation;
+		ent.UsePhysicsCollision = true;
+
+		ent.CopyFrom( this );
+		ent.CopyBonesFrom( this );
+		ent.SetRagdollVelocityFrom( this );
+
+		// Copy the clothes over
+		foreach ( var child in Children )
+		{
+			if ( !child.Tags.Has( "clothes" ) )
+				continue;
+
+			if ( child is ModelEntity e )
+			{
+				var clothing = new ModelEntity();
+				clothing.CopyFrom( e );
+				clothing.SetParent( ent, true );
+			}
+		}
+
+		ent.PhysicsGroup.AddVelocity( force );
+
+		if ( forceBone >= 0 )
+		{
+			var body = ent.GetBonePhysicsBody( forceBone );
+			if ( body != null )
+			{
+				body.ApplyForce( force * 1000 );
+			}
+			else
+			{
+				ent.PhysicsGroup.AddVelocity( force );
+			}
+		}
+
+		Corpse = ent;
+	}
+
+	[ClientRpc]
+	public void DestroyCorpse()
+	{
+		if ( !Corpse.IsValid() )
+			return;
+
+		Corpse.Delete();
+		Corpse = null;
+	}
+}
---
+++ code/PHGame.cs
@@ -0,0 +1,94 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public partial class PHGame : Game
+{
+	public static PHGame Instance { get; private set; } = Current as PHGame;
+
+	public PHGame()
+	{
+		if(IsServer)
+		{
+			
+		}
+
+		if ( IsClient )
+		{
+
+		}
+	}
+
+	[Event.Hotload]
+	public void Hotload()
+	{
+		
+	}
+
+	//Allow admins to use dev cam
+	public override void DoPlayerDevCam( Client client )
+	{
+		var camera = client.Components.Get<DevCamera>( true );
+
+		if ( camera == null )
+		{
+			camera = new DevCamera();
+			client.Components.Add( camera );
+			return;
+		}
+
+		camera.Enabled = !camera.Enabled;
+	}
+
+	//Allow admins to toggle noclipping
+	public override void DoPlayerNoclip( Client player )
+	{
+		if ( player.Pawn is Player basePlayer )
+		{
+			if ( basePlayer.DevController is NoclipController )
+			{
+				Log.Info( player.Name + " Noclip Mode Off" );
+				basePlayer.DevController = null;
+			}
+			else
+			{
+				Log.Info( player.Name + " Noclip Mode On" );
+				basePlayer.DevController = new NoclipController();
+			}
+		}
+	}
+
+	public override void PostLevelLoaded()
+	{
+		base.PostLevelLoaded();
+	}
+
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var pawn = new BasePawn();
+		pawn.Spawn();
+
+		client.Pawn = pawn;
+	}
+	public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
+	{
+		base.ClientDisconnect( cl, reason );
+	}
+
+	public override void Shutdown()
+	{	
+		base.Shutdown();
+	}
+
+	public override void DoPlayerSuicide( Client cl )
+	{
+		base.DoPlayerSuicide(cl);
+	}
+}
